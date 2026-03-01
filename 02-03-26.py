a0a5cdf183417b240f2b059a5f198968e0303518
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-03-02 00:59:05 +0530

    integers&Float

---
+++ 02-03-26.py
@@ -0,0 +1,48 @@
+# INTEGER & FLOAT
+
+num = 4.2
+
+print(num)
+print(type(num))
+
+#Arithmetic operators:
+# addition:      3+2
+#subtraction:    3-2
+#Multiplication: 3*2
+#Division:       3/2
+#floor Division:  3//2
+#Modulus:         3%2
+
+print( 3+2)
+print(3-2)
+print(3*2)
+print(3/2) #float value or decimal value
+print(3//2) #whole value
+print(3 ** 2) 
+print(3 % 2)
+
+
+#comparisons:
+#Equal:    3==2
+#Not equal:  3!=2 
+#Greater Than: 3 > 2 
+#less Than:    3<2
+#Greater or Equal: 3 >= 2
+#Less or Equal:   3 <= 2
+
+print(3 == 2)
+print(3 != 2)
+print(3 > 2)
+print(3 < 2)
+print(3 >= 2)
+print(3 <= 2)
+
+
+num_1 = '100'
+num_2 = '200'
+
+print(num_1 + num_2) # string concatination
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
