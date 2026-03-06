6f8f7260108ea85f4dd1f2a6bf62852ae64271fe
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-03-06 22:52:31 +0530

    import-export

---
+++ intro.py
@@ -0,0 +1,22 @@
+# importing file from same directory
+import my_module as mm
+
+# import system directory
+import sys
+
+# importing particular function
+from my_module import find_index , test
+courses = ['History','Math','Physics','CompSci']
+
+# this approch maybe only give us access to find index function not anything else
+# if we need more functions we can specify them separting through commas
+index = find_index(courses,'Physics')
+print(index)
+print(test)
+print(sys.path)
+
+# when we run sys.path we get information 
+# 1 directory that contains script we are running 
+# 2 python path environment variable
+# 3 python standard library directory
+# 4 site packages , or third party packages
