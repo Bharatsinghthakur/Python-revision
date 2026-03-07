51d800127d19beb5897869c58be8bcfe1a62c81e
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-03-07 19:37:25 +0530

    import-export

--- intro.py
+++ intro.py
@@ -1,15 +1,31 @@
 # importing file from same directory
 import my_module as mm
-
+import random
+import math
+import datetime
+import calendar
+import os
 # import system directory
 import sys
+
 
 # importing particular function
 from my_module import find_index , test
 
 # diff directory module 
 import myfile.import_module
+from myfile.import_module import my_function_import
 courses = ['History','Math','Physics','CompSci']
+random_course = random.choice(courses)
+print(random_course)
+
+rads = math.radians(90)
+print(math.sin(rads))
+
+today = datetime.date.today()
+print(today)
+
+print(calendar.isleap(2020))
 
 # this approch maybe only give us access to find index function not anything else
 # if we need more functions we can specify them separting through commas
@@ -23,3 +39,13 @@ print(sys.path)
 # 2 python path environment variable
 # 3 python standard library directory
 # 4 site packages , or third party packages
+
+
+''' we are importing our function from diffrent module'''
+my_function_import()
+
+
+print(os.getcwd())
+print(os.__file__)
+
+
--- myfile/import_module.py
+++ myfile/import_module.py
@@ -1,1 +1,5 @@
 print("this is import module in the directory")
+
+def my_function_import():
+   return print("YES !! we are getting imported function")
+
