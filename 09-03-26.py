51effdfb3ec596bf6c77a4f31239b6fc788e4632
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-03-10 01:56:43 +0530

    PIP-installer

---
+++ 09-03-26.py
@@ -0,0 +1,37 @@
+# PIP 
+
+"""
+commands for pip 
+
+pip help install
+
+pip help 
+
+pip search packagename
+pip search pympler
+
+pip list 
+-- for all the list of the packages
+
+pip uninstall pympler
+
+pip list -o 
+this command will compare your package version with 
+current package version 
+
+to install a package
+
+pip install -u setuptools
+
+##### 
+Now you want to provide the list of package 
+to your friend
+
+pip freeze 
+pip freeze > requirement.txt
+
+once you provide this they can install it like
+
+pip install -r r_test.txt
+
+"""
