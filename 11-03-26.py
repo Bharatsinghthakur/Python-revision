deced6e7bf8939172fc9599239abfee7caab40aa
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-03-11 21:44:08 +0530

    dict-reader-method

--- 11-03-26.py
+++ 11-03-26.py
@@ -9,21 +9,48 @@ import csv
 
 
 # Read the file as read more csv with reader
-with open('names.csv','r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    print(csv_reader)
+# with open('names.csv','r') as csv_file:
+#     csv_reader = csv.reader(csv_file)
+#     print(csv_reader)
 
     # next(csv_reader) 
     # next help us to step over our value in iterable 
     # we can use diffrent delimeter
-    with open('new_names.csv','w') as new_file:
-        csv_writer = csv.writer(new_file,delimiter='\t')
+    # with open('new_names.csv','w') as new_file:
+    #     csv_writer = csv.writer(new_file,delimiter='\t')
+        
+    #     for line in csv_reader:
+    #        csv_writer.writerow(line)
+
+
+# with open('new_names.csv','r') as csv_file:
+#     csv_reader = csv.reader(csv_file,delimiter='\t')
+   
+#     for line in csv_reader:
+#         print(line)
+
+
+# with open('names.csv','r') as csv_file:
+#     csv_reader = csv.DictReader(csv_file)
+
+#     for line in csv_reader:
+#         print(line['email'])
+
+with open('names.csv','r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+## we have to specify the filednames in dictionary for write 
+    with open('new_names.csv','w',newline="") as new_file:
+        fieldnames = ['first_name','last_name','email']
+        # if you want to remove the email 
+        
+        csv_writer = csv.DictWriter(new_file,fieldnames=fieldnames,delimiter='\t')
+        # we can specify the header name as well
+        
+        csv_writer.writeheader()
         
         for line in csv_reader:
-           csv_writer.writerow(line)
+           # we can remove the email as well
+            # del line['email']
+            csv_writer.writerow(line)
 
 
-with open('new_names.csv','r') as csv_file:
-    csv_reader = csv.reader(csv_file,)
-    for line in csv_reader:
-        print(line)
