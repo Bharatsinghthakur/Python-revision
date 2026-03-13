721f97763c29dab7bd2843deff5824423359d333
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-03-14 00:03:33 +0530

    instance_variable-class_variable

--- OOPS/13-03-26.py
+++ OOPS/13-03-26.py
@@ -1,14 +1,31 @@
 # Python Object-Oriented Programming
 
 class Employee:
+
+    raise_amount = 1.04
+    num_of_emps = 0
+
     def __init__(self,first,last,pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
+        Employee.num_of_emps += 1
    
     def fullname(self):
         return '{}{}'.format(self.first,self.last)
+    
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+        # both will work
+        # self.pay = int(self.pay * Employee.raise_amount)
+
+''' so whenever we want to access these class variable we need to acess them either through Class itself or 
+    instance of the class
+'''
+
+
+
 '''
 emp_1 = Employee()
 emp_2 = Employee()
@@ -38,4 +55,21 @@ print(class_emp1.fullname()) # not required anything because we are calling out 
 # here we are calling out class directory and then its method requires instance -- here its passed as self as an instance
 print(Employee.fullname(class_emp1)) # will require instance as argument to known for which we are calling out
 
-# lets create a methi
+print(class_emp1.apply_raise())
+
+
+# lets create how to access class variables
+
+print(class_emp1.raise_amount) # instance access
+print(Employee.raise_amount)   # class access
+
+
+# now check some important things
+print(class_emp1.__dict__)
+print(Employee.__dict__)
+
+# this will work because it will be created inside the instance of the class
+class_emp1.raise_amount = 1.05
+print(class_emp1.__dict__)
+
+print(Employee.num_of_emps)
