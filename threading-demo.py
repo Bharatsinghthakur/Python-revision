6f94dcad8614d4050b5586e2aac37f454b419533
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-03-24 22:56:33 +0530

    THREADING-THREADPOOLEXECUTOR

---
+++ threading-demo.py
@@ -0,0 +1,54 @@
+import threading
+
+import time
+
+start = time.perf_counter()
+
+
+def do_something(seconds):
+    print(f"sleeping {seconds} second(s)")
+    time.sleep(seconds)
+    print("Done sleeping")
+
+
+"""
+# we have to create thread we have 
+t1 = threading.Thread(target=do_something)
+t2 = threading.Thread(target=do_something)
+
+# we have to start the thread
+t1.start()
+t2.start()
+
+# do_something()
+# do_something()
+
+# As the function is getting finshed without calculating the time .
+# we can make the function wait till it complete its tread
+t1.join()
+t2.join()
+
+"""
+# list of threads so that we can store these and loop through
+
+threads = []
+
+# lets do this with more threads
+for _ in range(10):
+    t = threading.Thread(target=do_something,args=[1.5])
+    t.start()
+    # we cannot do join here as it will create the loop synchronously because its starting and running together without creating other threads
+    threads.append(t)
+
+# now we are running join on all thoose threads
+for thread in threads:
+    thread.join()
+
+finish = time.perf_counter()
+
+print(f"Finished in {round(finish-start,2)} second(s)")
+
+
+# efficient way of writing all this is 
+# Thread pool executor
+
---
+++ threadpoolexe.py
@@ -0,0 +1,47 @@
+import concurrent.futures
+import time
+
+start = time.perf_counter()
+
+def do_something(seconds):
+    print(f'Sleeping {seconds} Second(s)')
+    time.sleep(seconds)
+    return f'Done Sleeping ...{seconds}'
+
+'''
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+   secs = [5,4,3,2,1]
+#  results = [executor.submit(do_something,1) for _ in range(10)]
+   results = [executor.submit(do_something, sec) for sec in secs]
+
+   # will yield result and we can iterate it with loop
+   for f in concurrent.futures.as_completed(results):
+       print(f.result())
+
+    # we have list comprehension above for this
+       
+    # f1 = executor.submit(do_something,1) # future object
+    # f2 = executor.submit(do_something,1) 
+
+
+    # print(f1.result()) # it will wait around till the function complete
+    # print(f2.result()) # it will wait around till the function complete
+
+
+    # we can also use map function to do the same 
+    # map will return the result as in order the list values were passed
+'''
+
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    secs = [5,4,3,2,1]
+    results = executor.map(do_something,secs)
+   
+    for result in results:
+      print(result)
+
+finish = time.perf_counter()
+print(f"Finished in {round(finish-start,2)} second(s)")
+
+
