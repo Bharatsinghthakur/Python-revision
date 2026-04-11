ee0e5ac6fd8d5749a1751552da74712d99cfe2da
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-04-11 22:35:56 +0530

    Asyncio-coroutine-task-synchronous

--- asyncio_intro.py
+++ asyncio_intro.py
@@ -20,8 +20,32 @@ async def async_function(test_parm:str) -> str:
     return f"Async Result:{test_parm}"
 
 async def main():
-    sync_result = sync_function("Test")
-    print(sync_result)
+    # sync_result = sync_function("Test")
+    # print(sync_result)
+
+
+    # FUTURE -- promise like object used in low level code
+    # loop = asyncio.get_running_loop()
+    # future = loop.create_future() # a promise like object
+    # print(f"Empty Future: {future}")
+
+    # future.set_result("Future Result: Test")
+    # future_result = await future
+    # print(future_result)
+
+    # coroutine_obj = async_function("Test")
+    # print(coroutine_obj)
+
+    # coroutine_result = await coroutine_obj
+    # print(coroutine_result)
+
+    task = asyncio.create_task(async_function("Test"))
+    print(task)
+
+    task_result = await task 
+    print(task_result)
+
+
 
 # To start a event loop for any of our async code to work
 if __name__ == "__main__":
