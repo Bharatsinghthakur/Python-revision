6ed71ec1df4e86ae5ac2de344a4dd3d573c8fffc
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-05-14 23:26:03 +0530

    Getting started with DataAnalysis-pandas

--- pandas/lesson1.py
+++ pandas/lesson1.py
@@ -1,56 +1,8 @@
-import pandas as pd
- 
-lst = ['Geeks', 'For', 'Geeks', 'is', 
-            'portal', 'for', 'Geeks']
+# we need pandas to install pandas we have to run -- 
 
-df = pd.DataFrame(lst)
-print(df)
+#python -m pip install notebook  -- IT will install the notebook globally 
+                                    #or 
+# pip install notebook
 
+# to start the notebook we use  -- Jupyter notebook 
 
-import pandas as pd
- 
-data = {'Name':['Tom', 'nick', 'krish', 'jack'],
-        'Age':[20, 21, 19, 18]}
- 
-df = pd.DataFrame(data)
- 
-print(df)
-
-
-import pandas as pd
- 
-data = {'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'],
-        'Age':[27, 24, 22, 32],
-        'Address':['Delhi', 'Kanpur', 'Allahabad', 'Kannauj'],
-        'Qualification':['Msc', 'MA', 'MCA', 'Phd']}
- 
-df = pd.DataFrame(data)
- 
-print(df[['Name', 'Qualification']])
-
-import pandas as pd
-
- 
-dict = {'First Score':[100, 90, 85, 95],
-        'Second Score': [30, 45, 56, 40],
-        'Third Score':[55, 40, 80, 98]}
-
-df = pd.DataFrame(dict)
- 
-print(df.isnull())
-
-data = {
-  "calories": [420, 380, 390],
-  "duration": [50, 40, 45]
-}
-
-df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
-
-print(df) 
-
-
-#refer to the named index:
-print(df.loc["day2"])
-
-#refer to the named index:
-print(df.loc["day2"])
